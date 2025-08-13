07c03d9e9cd17cb76d6f24123411f47c25152ad4
HayzelBr <d2024013198@unifei.edu.br>
2025-08-13 19:59:39 -0300

    Primeiro commit

---
+++ ponteiro_seguro.h
@@ -0,0 +1,30 @@
+#ifndef SAFEPTR_H
+#define SAFEPTR_H
+
+using namespace std; 
+
+template <typename T>
+class SafePtr {
+
+    private:
+        T *ptr;
+
+    public:
+        SafePtr(T val){
+            ptr = new T{val};
+        }
+        ~SafePtr() {
+            delete ptr
+        }
+
+        T getValor() {
+            return *ptr;
+        }
+
+        long getEndereco(){
+            return (long)ptr;
+        }
+
+};
+
+#endif
